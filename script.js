b372345decaf539436bae6be01bc24b8de0f5c11
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-20 19:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	// Check if the key exists in the sampleObject
+  return key in sampleObject;
 }
 
 // Do not change the code below
